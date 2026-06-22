6bc90358f8b23b9bf019caa8dad6df39b47ffd9d
makss738 <maxence.monka@cyberv.fr>
2026-06-22 13:47:06 +0200

    Update tor.py

--- core/tor.py
+++ core/tor.py
@@ -1,1 +1,12 @@
+import requests
 
+proxies = {
+    "http": "socks5h://127.0.0.1:9050",
+    "https": "socks5h://127.0.0.1:9050"
+}
+
+print("[+] Testing Tor IP...")
+
+print(
+    requests.get("http://icanhazip.com", proxies=proxies).text.strip()
+)
